57fa8387acb659f89336d1baa7085d5d03613f47
Punnary12 <52101396+Punnary12@users.noreply.github.com>
2024-09-16 16:19:43 +0530

    Branch Protection
    Branch Protection

--- WebApplication25/Program.cs
+++ WebApplication25/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Devops Application5!");
+app.MapGet("/", () => "Devops Application6!");
 
 app.Run();
