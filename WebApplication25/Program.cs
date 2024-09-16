f92bc4cbf0906234196a5f7a033d8b0b21e97d10
Punnary Menon <punnary.menon@yahoo.com>
2024-09-16 15:07:25 +0530

    changes

--- WebApplication25/Program.cs
+++ WebApplication25/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Devops Application2!");
+app.MapGet("/", () => "Devops Application21!");
 
 app.Run();
